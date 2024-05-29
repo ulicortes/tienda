480e9e356d54a770070a3dc57ff49c012cd70778
ulicortes <ulisestcortes@gmail.com>
2024-05-29 11:25:06 -0300

    Carrito agregado al navbar, input creado y un par de detalles css.

--- src/app/componentes/detalle-elemento/detalle-elemento.component.ts
+++ src/app/componentes/detalle-elemento/detalle-elemento.component.ts
@@ -11,33 +11,8 @@ import { ListaElementosComponent } from '../lista-elementos/lista-elementos.comp
   styleUrl: './detalle-elemento.component.scss'
 })
 export class DetalleElementoComponent {
-  id: number;
-  carro: Elemento[] = [];
 
   @Input()
   elemento: Elemento;
 
-  constructor(private carrito: ServicioCarritoService, private route: ActivatedRoute, private apiElementos: ElementosService) {
-    carrito.carrito.subscribe((observable) => this.carro = observable);
-  }
-
-  // ngOnInit(): void {
-  //   this.id = parseInt(this.route.snapshot.paramMap.get('id')!)-1;
-  //   this.apiElementos.listaCompleta().subscribe(elem => this.elemento = elem[this.id]);
-  // }
-
-  aumentar(e: Elemento) {
-    if (e.cant_a_comprar < e.cant_disponible) e.cant_a_comprar++;
-  }
-  disminuir(e: Elemento) {
-    if (e.cant_a_comprar > 0) e.cant_a_comprar--;
-  }
-
-  agregarAlCarrito(elemento: Elemento): void {
-    if(elemento.cant_a_comprar <= elemento.cant_disponible) {
-      this.carrito.agregar(elemento);
-      elemento.cant_disponible -= elemento.cant_a_comprar;
-      elemento.cant_a_comprar = 0;
-    }
-  }
 }
--- src/app/componentes/input-cantidad/input-cantidad.component.ts
+++ src/app/componentes/input-cantidad/input-cantidad.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Elemento } from '../../interfaces/elemento';
+import { ServicioCarritoService } from '../../servicios/carrito.service';
 
 @Component({
   selector: 'app-input-cantidad',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './input-cantidad.component.scss'
 })
 export class InputCantidadComponent {
+  carro: Elemento[] = [];
 
+  @Input()
+  elemento: Elemento;
+
+  constructor(private carrito: ServicioCarritoService) {
+    carrito.carrito.subscribe((observable) => this.carro = observable);
+  }
+
+  aumentar(e: Elemento) {
+    if (e.cant_a_comprar < e.cant_disponible) e.cant_a_comprar++;
+  }
+  disminuir(e: Elemento) {
+    if (e.cant_a_comprar > 0) e.cant_a_comprar--;
+  }
+
+  agregarAlCarrito(elemento: Elemento): void {
+    if(elemento.cant_a_comprar <= elemento.cant_disponible) {
+      this.carrito.agregar(elemento);
+      elemento.cant_disponible -= elemento.cant_a_comprar;
+      elemento.cant_a_comprar = 0;
+    }
+  }
 }
